e835e0d43d94c813f0603baad9daa78206051778
cse-ritika <cse-ritika-thakur@gmail.com>
2026-04-21 11:39:54 +0530

    Added React files

--- React/controlled-component/controlledcomponent/src/App.jsx
+++ React/controlled-component/controlledcomponent/src/App.jsx
@@ -3,7 +3,9 @@ import React, { useState } from 'react'
 const App = () => {
     let [data, setData] = useState({
         username : '',
-        password : ''
+        password : '',
+        gender : '',
+        skills : []
     })
     let handleChange = (e) => {
         // console.log(e.target.value);
@@ -13,17 +15,33 @@ const App = () => {
             [name]:value
         })
     }
+    let [check, setChecked] = useState([])
+    let handleCheckBox = (e) => {
+      console.log(e);
+      
+      let {name , value} = e.target
+      setChecked({...check, [name]:value});
+
+      {
+        
+      }
+    }
   return (
     <div>
       <form action="">
         <input type="text" name='username' id='username' value={data.username} placeholder='username' onChange={handleChange}/> <br /><br />
       <input type="text" name='password' id='password' value={data.password} placeholder='password' onChange={handleChange}/> <br /><br />
       Male <input type="radio" name="gender" id="" value='Male' onChange={handleChange}/>
-      Female <input type="radio" name="gender" id="" value='Female' onChange={handleChange}/>
+      Female <input type="radio" name="gender" id="" value='Female' onChange={handleChange}/> <br /><br />
+
+      HTML<input type="checkbox" name="skills" id="" value='html' onChange={handleCheckBox} />
+      CSS<input type="checkbox" name="skills" id="" value='css' onChange={handleCheckBox} />
       </form>
       <hr />
         <h1>Username : {data.username}</h1>
         <h1>Password : {data.password}</h1>
+        <h1>Gender : {data.gender}</h1>
+        <h1>Skills : {data.skills.join(', ')}</h1>
     </div>
   )
 }
